c04afa52a222273e9d83dbd5900a9a00a70c8bcd
Andrea Sabatini <sabatini.andrea@outlook.it>
2021-03-25 18:22:50 +0100

    Primi passi JS

--- master.js
+++ master.js
@@ -1,12 +1,36 @@
 var app = new Vue({
 
-        el: '#root',
-        data: {
+    el: '#root',
+    data: {
+        input: '',
+        todos: [
+            {
+                title: 'Completare esercitazione di ieri',
+            },
+            {
+                title: 'Andare al supermercato',
+            },
+            {
+                title: 'Riunione alle ore 9:00',
+            },
+            {
+                title: 'Cena da nonna',
+            },
+            {
+                title: 'Annaffiare la piante',
+            },
+        ],
+    },
 
-
-        },
-        methods: {
-
-
+    methods: {
+        add: function () {
+            if (this.input != '') {
+                let obj = {
+                    title: this.input
+                }
+                this.todos.push(obj);
+                this.input = '';
+            }
         }
+    }
 });
